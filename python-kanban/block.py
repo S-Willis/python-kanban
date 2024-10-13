75cef93e62410cdda57352ca03a2ad82f7d996d3
Sky Willis <sky.willis@outlook.com>
2024-10-13 16:52:36 +0100

    add TODO functions in block.py

--- python-kanban/block.py
+++ python-kanban/block.py
@@ -16,6 +16,7 @@ class Block(db.model):
     blockee_id = db.Column(db.Integer)
     blocker_id = db.Column(db.Integer, default=0)
     title = db.Column(db.String(120), default="Blocker")
+    cleared = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         """Return a string representation of a Block Instance"""
@@ -30,3 +31,33 @@ class Block(db.model):
             'blocker_id': self.blocker_id,
         }
 
+def all_blocks():
+    """Return JSON for all blocks"""
+    return [block.json() for block in Block.query.all()]
+
+def create_block(title, **kwargs):
+    """Create a new Block"""
+    #TODO: Auto-pull block title from blocker card?
+    db.session.add(Block(title=title, **kwargs))
+    db.session.commit()
+
+def delete_block(block_id):
+    """Delete a block"""
+    db.session.delete(Block.query.get(block_id))
+    db.session.commit()
+
+def clear_block(block_id):
+    """Clear a block, removing it from UI but keeping it in DB"""
+    block = Block.query.get(block_id)
+
+    block.cleared = True
+
+    db.session.commit()
+
+def get_blockers_for_card(blockee_id):
+    #TODO: Return list of Blocks (Cards?) where blockee_id matches
+    return
+
+def get_cards_blocked_by(blocker_id):
+    #TODO: Return list of Blocks (Cards?) where blocker_id matches
+    return
